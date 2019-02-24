fdcb949351f3657ada6fa026b9a33dbe402c86b8
archana-vish <archana-vish@yahoo.in>
2019-02-24 21:40:08 +0000

    How this compares to java sytax

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,3 +1,9 @@
+//package name
+
+// Below is similar to import java.util.list; 
+// So from ES6 onwards, you can use 'classes' in js
+// Now each module is a collection of classes and function definitions
+// You can import the needed modules as per the below syntax
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +11,20 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
+
+// In Java we have access modifiers like public indicates where this class can be used
+// export is similar to an access modifier that denotes that this class can be used outside the boundary of the module
 export class WelcomeComponent implements OnInit {
 
+  // Member variables
+  message: string = 'hello world';
+
+  // aha! a constructor duh!
   constructor() { }
 
+  // method implemented from OnInit
   ngOnInit() {
+    this.message = ' how are you..';
   }
 
 }
